97c4f8ddc6d345f874c22a53dab4ab10a1651920
Zakhar Pochuvalov <94977621+sobselena@users.noreply.github.com>
2026-04-01 19:19:26 +0300

    feat: add Mantine UI and configure basic fonts

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,4 +1,15 @@
+import '@mantine/core/styles.css';
+
+import { MantineProvider } from '@mantine/core';
 import { RouterProvider } from 'react-router';
 import { router } from './router';
 
-export const App = () => <RouterProvider router={router} />;
+export const App = () => (
+  <MantineProvider
+    theme={{
+      fontFamily: 'Inter, sans-serif',
+    }}
+  >
+    <RouterProvider router={router} />
+  </MantineProvider>
+);
